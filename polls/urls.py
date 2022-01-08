44501dde079e4ebd85a346253c2d9117a69b2892
LAPTOP-74FCSJHK\liuzh <liuzhengyang689@gmail.com>
2022-01-08 19:23:10 +0900

    更新

--- polls/urls.py
+++ polls/urls.py
@@ -4,12 +4,14 @@ from . import views
 
 app_name = 'polls'
 urlpatterns = [
+    # index画面
     path('', views.IndexView.as_view(), name='index'),
     # 新規作成画面遷移
     path('create/', views.ToCreate, name='create'),
     path('create/regist/', views.regist, name='regist'),
+    path('<int:question_id>/update/', views.update, name='update'),
     path('<int:question_id>/', views.detail, name='detail'),
     path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
-    path('<int:pk>/edit/', views.EditView.as_view(), name='edit'),
+    path('<int:question_id>/edit/', views.edit, name='edit'),
     path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
--- polls/views.py
+++ polls/views.py
@@ -8,6 +8,7 @@ from django.db import transaction
 
 from .models import Choice, Question
 
+# 一覧画面遷移
 class IndexView(generic.ListView):
     template_name = 'polls/index.html'
     context_object_name = 'latest_question_list'
@@ -19,25 +20,43 @@ class IndexView(generic.ListView):
         """
         return Question.objects.filter(
             pub_date__lte=timezone.now()
-        ).order_by('-pub_date')[:5]
+        ).order_by('-pub_date')[:10]
 
+# 詳細画面遷移
 def detail(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
-    vote_start = False
-    for choice in question.choice_set.all():
-        if choice.votes > 0:
-            vote_start = True
-            break
+    vote_start = get_vote_start(question_id)
 
     return render(request, 'polls/detail.html', {
         'question': question,
         'vote_start':vote_start,
         })
 
+# 投票開始かを判断するメソッド
+def get_vote_start(question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    vote_start = False
+    for choice in question.choice_set.all():
+        if choice.votes > 0:
+            vote_start = True
+            break
+    return vote_start
 
-class EditView(generic.DetailView):
-    model = Question
-    template_name = 'polls/edit.html'
+# 編集画面遷移
+def edit(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    choices_q = question.choice_set.all()
+    choices=[]
+    for choice in choices_q:
+        choices.append(choice)
+    count_len = len(choices)
+    for count_len in range(count_len,4):
+        choices.append("")
+    print(question)
+    return render(request, 'polls/edit.html', {
+        'question': question,
+        'Choices':choices,
+        })
 
 class ResultsView(generic.DetailView):
     model = Question
@@ -53,9 +72,11 @@ def vote(request, question_id):
         selected_choice = question.choice_set.get(pk=request.POST['choice'])
     except (KeyError, Choice.DoesNotExist):
         # Redisplay the question voting form.
+        vote_start = get_vote_start(question_id)
         return render(request, 'polls/detail.html', {
             'question': question,
             'error_messages': "１つを選択してください。",
+            'vote_start':vote_start,
         })
     else:
         selected_choice.votes += 1
@@ -65,7 +86,7 @@ def vote(request, question_id):
         # user hits the Back button.
         return HttpResponseRedirect(reverse('polls:detail', args=(question.id,)))
 
-# 投票結果
+# 投票結果（使わない）
 def results(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     return render(request, 'polls/detail.html', {
@@ -89,6 +110,9 @@ def regist(request):
             return HttpResponseRedirect(reverse('polls:index'))
     error_messages = []
     myQuestion1 = request.POST.get('myQuestion1', "")
+    double_check=Question.objects.filter(question_text=myQuestion1).count()
+    if double_check > 0:
+        error_messages.append("この質問既に存在しています。")
     if len(myQuestion1.strip()) ==0:
         error_messages.append("質問を入力してください。")
     choices = []
@@ -124,3 +148,62 @@ def regist(request):
                 q.choice_set.create(choice_text=choice, votes=0)
         
     return HttpResponseRedirect(reverse('polls:index'))
+
+# 編集機能
+@transaction.atomic 
+def update(request, question_id):
+    if request.method == 'POST':
+        if 'goBack' in request.POST:
+            question = get_object_or_404(Question, pk=question_id)
+            vote_start = get_vote_start(question_id)
+            return render(request, 'polls/detail.html', {
+                            'question': question,
+                            'vote_start':vote_start,
+                        })
+    error_messages = []
+    myQuestion1 = request.POST.get('myQuestion1', "")
+    if len(myQuestion1.strip()) ==0:
+        error_messages.append("質問を入力してください。")
+
+    double_check=Question.objects.exclude(pk=question_id).filter(question_text=myQuestion1).count()
+    print(double_check)
+    if double_check > 0:
+        error_messages.append("この質問既に存在しています。")
+    choices = []
+    count = 0
+    duplicate = False
+    duplicate_choice = []
+    choices.append(request.POST.get('myChoice1', ""))
+    choices.append(request.POST.get('myChoice2', ""))
+    choices.append(request.POST.get('myChoice3', ""))
+    choices.append(request.POST.get('myChoice4', ""))
+    
+    for choice in choices:
+        if len(choice.strip()) !=0:
+            count += 1
+            duplicate_choice.append(choice)
+    duplicate = has_duplicates(duplicate_choice)
+    if duplicate is True:
+        error_messages.append("選択肢を重複しないように登録してください。")
+    
+    if count < 2:
+        error_messages.append("選択肢を２つ以上入力してください。")
+    
+    if len(error_messages) > 0:
+        question = get_object_or_404(Question,pk=question_id)
+        return render(request, 'polls/edit.html', {
+            'error_messages': error_messages,
+            'question':question,
+            'Choices':choices,
+        })
+    if len(error_messages) == 0:
+        question_old = get_object_or_404(Question, pk=question_id)
+        question_old.question_text=myQuestion1
+        Choice.objects.filter(question=question_old).delete()
+        question_old.save()
+        for choice in choices:
+            if len(choice.strip()) !=0:
+                question_old.choice_set.create(choice_text=choice, votes=0)
+        
+    return HttpResponseRedirect(reverse('polls:index'))
+
